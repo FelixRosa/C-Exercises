b56a247f9c2595a3c46f3d911d11ae520658fb58
Felix Rosa <interweb@frwv.de>
2019-05-30 21:33:31 +0200

    Fourth Exercise

--- Exercise 5 - Files/ProgLogic.cs
+++ Exercise 5 - Files/ProgLogic.cs
@@ -1,19 +1,36 @@
 using System;
+using System.IO;
+using System.Linq;
 
 namespace Exercise_5___Files
 {
     public class ProgLogic
     {
         //1- Write a program that reads a text file and displays the number of words. 
-        public static String WordsInTextFile()
+        public static String WordsInTextFile(String path)
         {
-            return null;
+            //var fileText = File.ReadAllText(path);
+            var fileText = File.ReadAllText(path);
+            var words = fileText.ToString().Split(" ");
+            //Only necessray with .rft files - skips the settings
+            words = words.Skip(3).ToArray();
+            return String.Format("Amount of Words: {0}", words.Length);
         }
 
         //2- Write a program that reads a text file and displays the longest word in the file. 
-        public static String LongestWordInTextFile()
+        public static String LongestWordInTextFile(String path)
         {
-            return null;
+            var fileText = File.ReadAllText(path);
+            var parsedFile = fileText.ToString().Split(" ");
+            parsedFile = parsedFile.Skip(3).ToArray();
+            //Only necessray with .rft files - skips the settings
+            var longestWord = parsedFile[0];
+            foreach (var word in parsedFile)
+            {
+                if (word.Length > longestWord.Length)
+                    longestWord = word;
+            }
+            return String.Format("The longest word in the file is: {0}", longestWord);
         }
     }
 }
--- Exercise 5 - Files/Program.cs
+++ Exercise 5 - Files/Program.cs
@@ -6,12 +6,14 @@ namespace Exercise_5___Files
     {
         static void Main(string[] args)
         {
+            var path = @"/Users/felixrosa/documents/text2.rtf";
+            
             Console.WriteLine("Get the Amount of Words in a Text-File!");
-            Console.WriteLine(ProgLogic.WordsInTextFile());
+            Console.WriteLine(ProgLogic.WordsInTextFile(path));
             Console.WriteLine("\n");
 
             Console.WriteLine("Get the longest word in a Text-File!");
-            Console.WriteLine(ProgLogic.LongestWordInTextFile());
+            Console.WriteLine(ProgLogic.LongestWordInTextFile(path));
             
         }
     }
