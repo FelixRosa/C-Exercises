eef614f4201b0f5f4af55256c004c367df0761d2
Felix Rosa <interweb@frwv.de>
2019-05-30 19:21:01 +0200

    Fourth Exercise

---
+++ Exercise 5 - Files/ProgLogic.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Exercise_5___Files
+{
+    public class ProgLogic
+    {
+        //1- Write a program that reads a text file and displays the number of words. 
+        public static String WordsInTextFile()
+        {
+            
+        }
+
+        //2- Write a program that reads a text file and displays the longest word in the file. 
+        public static String LongestWordInTextFile()
+        {
+            
+        }
+    }
+}
---
+++ Exercise 5 - Files/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_5___Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Get the Amount of Words in a Text-File!");
+            Console.WriteLine(ProgLogic.WordsInTextFile());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Get the longest word in a Text-File!");
+            Console.WriteLine(ProgLogic.LongestWordInTextFile());
+            
+        }
+    }
+}
