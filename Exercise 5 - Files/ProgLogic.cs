91e43854c3ca62e9328e4c83d0294840e424a86b
Felix Rosa <interweb@frwv.de>
2019-05-30 19:21:29 +0200

    Fourth Exercise

--- Exercise 5 - Files/ProgLogic.cs
+++ Exercise 5 - Files/ProgLogic.cs
@@ -7,13 +7,13 @@ namespace Exercise_5___Files
         //1- Write a program that reads a text file and displays the number of words. 
         public static String WordsInTextFile()
         {
-            
+            return null;
         }
 
         //2- Write a program that reads a text file and displays the longest word in the file. 
         public static String LongestWordInTextFile()
         {
-            
+            return null;
         }
     }
 }
